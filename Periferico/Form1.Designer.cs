a1931af9e92b4b5242cdec4b99439a015b5e488c
FelipeChicarelli <68930292+FelipeChicarelli@users.noreply.github.com>
2024-09-16 21:18:24 -0300

    Add files via upload

---
+++ Periferico/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Periferico
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            txtNome = new TextBox();
+            txtCor = new TextBox();
+            txtData = new TextBox();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(26, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Cor";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 120);
+            label3.Name = "label3";
+            label3.Size = new Size(91, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Data Fabricacao";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 18);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Periferico Id";
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(124, 44);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(318, 23);
+            txtNome.TabIndex = 4;
+            // 
+            // txtCor
+            // 
+            txtCor.Location = new Point(124, 79);
+            txtCor.Name = "txtCor";
+            txtCor.Size = new Size(318, 23);
+            txtCor.TabIndex = 5;
+            // 
+            // txtData
+            // 
+            txtData.Location = new Point(124, 112);
+            txtData.Name = "txtData";
+            txtData.Size = new Size(318, 23);
+            txtData.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(124, 18);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 15);
+            label5.TabIndex = 7;
+            label5.Click += label5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(629, 295);
+            Controls.Add(label5);
+            Controls.Add(txtData);
+            Controls.Add(txtCor);
+            Controls.Add(txtNome);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "Periferico";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtNome;
+        private TextBox txtCor;
+        private TextBox txtData;
+        private Label label5;
+    }
+}
---
+++ Periferico/conexaoBanco.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perifericos
+{
+    static class ConexaoBanco
+    {
+        //4 Variaveis declaradas passando as informações do banco de dados
+        private const string servidor = "localhost";
+        private const string bancoDados = "dbperiferico";
+        private const string usuario = "root";
+        private const string senha = "Ch1c@r3ll1";
+
+        //Declarando Variavel conexaoBanco para fazer a conexao com o banco de dados
+        static public string bancoServidor = $"server={servidor}; user id ={usuario}; database ={bancoDados}; password={senha}";
+
+    }
+}
